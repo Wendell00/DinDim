329baf9e5a2bd55c80be06a67820c48a13bc6218
Wendell00 <wendellborges2003@gmail.com>
2023-12-31 11:14:18 -0300

    :sparkles: feat: Conectando com o MongoDB

--- src/app/database/database.ts
+++ src/app/database/database.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 mongoose.set('strictQuery', true)
 
 const dbname = "dindim"
-const url = `mongodb+srv://wendelldev353:${process.env.DB_PWD}@cluster0.dcfurg5.mongodb.net/${dbname}?retryWrites=true&w=majority`
+const url = `mongodb+srv://wendelldev353:${process.env.MONGO_PWD}@cluster0.dcfurg5.mongodb.net/${dbname}?retryWrites=true&w=majority`
 
 const connect = async () => {
     return await mongoose.connect(url)
